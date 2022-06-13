2a147d071d29f374b80de0ce6df6e3d0b58598f2
Louis Gallucci <lou.gallucci86@gmail.com>
2022-06-13 00:35:48 -0500

    import localizations from atlas

--- AddonManager/FileManagers/LocalizationFileManager.cs
+++ AddonManager/FileManagers/LocalizationFileManager.cs
@@ -28,9 +28,11 @@ public static class LocalizationFileManager
         var localList = localizations.ToList();
         localList.Sort();
 
-        var localizedLanguages = new[] { "esES", "esMX", "deDE", "frFR", "ruRU", "zhCN", "koKR", "zhTW" };
+        var localizedLanguages = new []{ "esES", "esMX", "deDE", "frFR", "ruRU", "zhCN", "koKR", "zhTW" };
+
         foreach (var language in localizedLanguages)
         {
+            var foundLocalization = FindLocalizations(language);
             var translatedLocalizations = GetExistingTranslations(language);
             var fileText = $"if GetLocale() == \"{language}\" then\n"; 
             
@@ -39,6 +41,10 @@ public static class LocalizationFileManager
                 if (translatedLocalizations.ContainsKey(localization))
                 {
                     fileText += translatedLocalizations[localization] + "\n";
+                } 
+                else if (foundLocalization.ContainsKey(localization))
+                {
+                    fileText += $"  LBIS.L[\"{localization}\"] = \"{foundLocalization[localization]}\";\n";
                 } 
                 else
                 {
@@ -50,6 +56,141 @@ public static class LocalizationFileManager
 
             System.IO.File.WriteAllText(@$"..\..\..\..\LoonBestInSlot\Localization\localization.{language}.lua", fileText);
         }
+    }
+
+    private static Dictionary<string, string> FindLocalizations(string language)
+    {
+        var localizations = new Dictionary<string, string>();
+
+        //Search in AtlasLootClassic
+        var alcPath = $@"E:\Blizzard\World of Warcraft\_classic_\Interface\Addons\AtlasLootClassic_DungeonsAndRaids\Locales\";
+        switch(language)
+        {
+            case "esES":
+                alcPath += "constants.es.lua";
+                break;
+            case "esMX":
+                alcPath += "constants.mx.lua";
+                break;
+            case "deDE":
+                alcPath += "constants.de.lua";
+                break;
+            case "frFR":
+                alcPath += "constants.fr.lua";
+                break;
+            case "ruRU":
+                alcPath += "constants.ru.lua";
+                break;
+            case "zhCN":
+                alcPath += "constants.cn.lua";
+                break;
+            case "koKR":
+                alcPath += "constants.kr.lua";
+                break;
+            case "zhTW":
+                alcPath += "constants.tw.lua";
+                break;
+        }
+
+        string[] itemSources = System.IO.File.ReadAllLines(alcPath);
+        foreach(var line in itemSources)
+        {
+            if (!line.StartsWith("AL["))
+                continue;
+
+            var localizedString = string.Empty;
+            var position = 3;
+            while (position <= line.Length && line[position] != ']')
+            {
+                localizedString += line[position];
+                position++;
+            }
+
+            var translation = line.Split("=")[1].Trim().Trim(',').Trim('\"');
+            localizations.Add(localizedString.Trim('\"'), translation);
+        }
+
+        //Search in LibBabbleSubZone
+        alcPath = $@"E:\Blizzard\World of Warcraft\_classic_\Interface\Addons\Atlas\Libs\LibBabble-SubZone-3.0\";
+        switch (language)
+        {
+            case "esES":
+                alcPath += "LibBabble-SubZone-3.0_esES.lua";
+                break;
+            case "esMX":
+                alcPath += "LibBabble-SubZone-3.0_esMX.lua";
+                break;
+            case "deDE":
+                alcPath += "LibBabble-SubZone-3.0_deDE.lua";
+                break;
+            case "frFR":
+                alcPath += "LibBabble-SubZone-3.0_frFR.lua";
+                break;
+            case "ruRU":
+                alcPath += "LibBabble-SubZone-3.0_ruRU.lua";
+                break;
+            case "zhCN":
+                alcPath += "LibBabble-SubZone-3.0_zhCN.lua";
+                break;
+            case "koKR":
+                alcPath += "LibBabble-SubZone-3.0_koKR.lua";
+                break;
+            case "zhTW":
+                alcPath += "LibBabble-SubZone-3.0_zhTW.lua";
+                break;
+        }
+
+        itemSources = System.IO.File.ReadAllLines(alcPath);
+        foreach (var line in itemSources)
+        {
+            if (!line.StartsWith("	["))
+                continue;
+
+            var localizedString = string.Empty;
+            var position = 2;
+            while (position <= line.Length && line[position] != ']')
+            {
+                localizedString += line[position];
+                position++;
+            }
+
+            var translation = line.Split("=")[1].Trim().Trim(',').Trim('\"');
+            if (!localizations.ContainsKey(localizedString.Trim('\"')))
+                localizations.Add(localizedString.Trim('\"'), translation);
+        }
+
+        //Search in LibBabbleBoss
+        alcPath = $@"E:\Blizzard\World of Warcraft\_classic_\Interface\Addons\Atlas\Libs\LibBabble-Boss-3.0\LibBabble-Boss-3.0.lua";
+        itemSources = System.IO.File.ReadAllLines(alcPath);
+        var foundStart = false;
+        foreach (var line in itemSources)
+        {
+            if (!foundStart && !line.StartsWith("elseif"))
+                continue;
+
+            if (foundStart && line.StartsWith("elseif"))
+                break;
+
+            if (line.StartsWith("elseif") && line.Contains(language))
+                foundStart = true;
+
+            if (!foundStart || !line.StartsWith("	["))
+                continue;
+
+            var localizedString = string.Empty;
+            var position = 2;
+            while (position <= line.Length && line[position] != ']')
+            {
+                localizedString += line[position];
+                position++;
+            }
+
+            var translation = line.Split("=")[1].Trim().Trim(',').Trim('\"');
+            if (!localizations.ContainsKey(localizedString.Trim('\"')))
+                localizations.Add(localizedString.Trim('\"'), translation);
+        }
+
+        return localizations;
     }
 
     private static Dictionary<string, string> GetExistingTranslations(string language)
