b1b7a74bd4dba9333fd8bfe620c01430cdcc6996
Louis Gallucci <lou.gallucci86@gmail.com>
2022-09-01 02:49:35 -0500

    update warrior gem/enchants

--- LoonBestInSlot/Guides/GemsAndEnchants/WarriorProtection.lua
+++ LoonBestInSlot/Guides/GemsAndEnchants/WarriorProtection.lua
@@ -1,1 +1,28 @@
 local spec = LBIS:RegisterSpec(LBIS.L["Warrior"], LBIS.L["Protection"], "0")
+LBIS:AddGem(spec, "41380", "True") --Austere Earthsiege Diamond
+LBIS:AddGem(spec, "40008", "False") --Solid Sky Sapphire
+LBIS:AddGem(spec, "45880", "False") --Solid Stormjewel
+LBIS:AddGem(spec, "40034", "False") --Guardian's Twilight Opal
+LBIS:AddGem(spec, "49110", "False") --Nightmare Tear
+LBIS:AddGem(spec, "40141", "False") --Guardian's Dreadstone
+
+
+LBIS:AddGem(spec, "50369", LBIS.L["Head"], "False") --Arcanum of the Stalwart Protector
+LBIS:AddGem(spec, "67839", LBIS.L["Head"], "True") --Mind Amplification Dish
+
+LBIS:AddGem(spec, "44957", LBIS.L["Shoulder"], "False") --Greater Inscription of the Gladiator
+
+LBIS:AddGem(spec, "62256", LBIS.L["Wrist"], "True") --Enchant Bracers - Major Stamina
+
+LBIS:AddGem(spec, "63770", LBIS.L["Hands"], "True") --Reticulated Armor Webbing
+LBIS:AddGem(spec, "34207", LBIS.L["Hands"], "False") --Glove Reinforcements
+LBIS:AddGem(spec, "38376", LBIS.L["Hands"], "False") --Heavy Borean Armor Kit
+LBIS:AddGem(spec, "44625", LBIS.L["Hands"], "True") --Enchant Gloves - Armsman
+
+LBIS:AddGem(spec, "38373", LBIS.L["Legs"], "False") --Frosthide Leg Armor
+LBIS:AddGem(spec, "38371", LBIS.L["Legs"], "False") --Jormungar Leg Armor
+
+LBIS:AddGem(spec, "59636", LBIS.L["Ring"], "True") --Enchant Ring - Stamina
+
+LBIS:AddGem(spec, "41167", LBIS.L["Ranged"], "False") --Heartseeker Scope
+
