2e1802799ebebe159227cc9737244335d75dbf50
Louis Gallucci <lou.gallucci86@gmail.com>
2022-09-11 18:15:35 -0500

    update paladin prot

--- LoonBestInSlot/Guides/Phase0/PaladinProtection.lua
+++ LoonBestInSlot/Guides/Phase0/PaladinProtection.lua
@@ -6,7 +6,7 @@ LBIS:AddItem(spec, "37633", LBIS.L["Head"], "BIS") --Ground Tremor Helm
 LBIS:AddItem(spec, "37135", LBIS.L["Head"], "Alt") --Arcane-Shielded Helm
 LBIS:AddItem(spec, "36969", LBIS.L["Head"], "Alt") --Helm of the Ley-Guardian
 LBIS:AddItem(spec, "34401", LBIS.L["Head"], "Alt") --Helm of Uther's Resolve
-LBIS:AddItem(spec, "34243", LBIS.L["Head"], "Transmute BIS") --Helm of Burning Righteousness
+LBIS:AddItem(spec, "34243", LBIS.L["Head"], "Transmute Alt") --Helm of Burning Righteousness
 LBIS:AddItem(spec, "44040", LBIS.L["Head"], "Alt") --The Crusader's Resolution
 LBIS:AddItem(spec, "40673", LBIS.L["Head"], "Alt") --Tempered Saronite Helm
 
@@ -51,7 +51,7 @@ LBIS:AddItem(spec, "43500", LBIS.L["Legs"], "BIS") --Bolstered Legplates
 LBIS:AddItem(spec, "37292", LBIS.L["Legs"], "Alt") --Ley-Guardian's Legguards
 LBIS:AddItem(spec, "44240", LBIS.L["Legs"], "Alt") --Special Issue Legplates
 LBIS:AddItem(spec, "34382", LBIS.L["Legs"], "Alt") --Judicator's Legguards
-LBIS:AddItem(spec, "34167", LBIS.L["Legs"], "Transmute BIS") --Legplates of the Holy Juggernaut
+LBIS:AddItem(spec, "34167", LBIS.L["Legs"], "Transmute Alt") --Legplates of the Holy Juggernaut
 LBIS:AddItem(spec, "37688", LBIS.L["Legs"], "Alt") --Legplates of the Infinite Drakonid
 
 LBIS:AddItem(spec, "44243", LBIS.L["Feet"], "BIS") --Toxin-Tempered Sabatons
