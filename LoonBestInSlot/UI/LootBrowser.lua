82e84dfba4d350208333bede3b864b5fca55c88a
Louis Gallucci <lou.gallucci86@gmail.com>
2023-09-25 17:02:59 -0500

    Gettin ready for phase 4

--- LoonBestInSlot/UI/LootBrowser.lua
+++ LoonBestInSlot/UI/LootBrowser.lua
@@ -278,7 +278,7 @@ local function createDropDowns(window)
         ['name']='phase',
         ['parent']=window,
         ['title']='Phase:',
-        ['items']= { LBIS.L["All"], LBIS.L["PreRaid"], LBIS.L["Phase 1"], LBIS.L["Phase 2"], LBIS.L["Phase 3"], "BIS" }, --LBIS.L["Phase 4"], LBIS.L["Phase 5"],
+        ['items']= { LBIS.L["All"], LBIS.L["PreRaid"], LBIS.L["Phase 1"], LBIS.L["Phase 2"], LBIS.L["Phase 3"], LBIS.L["Phase 4"], "BIS" }, --LBIS.L["Phase 5"],
         ['defaultVal']=LBISSettings.SelectedPhase,
         ['changeFunc']=function(dropdown_frame, dropdown_val)
             LBISSettings.SelectedPhase = dropdown_val;
--- LoonBestInSlot/UpdateTooltips.lua
+++ LoonBestInSlot/UpdateTooltips.lua
@@ -32,11 +32,11 @@ local function isInEnabledPhase(phaseText)
 			return true;
 		end
 	end
-	-- if LBISSettings.PhaseTooltip[LBIS.L["Phase 4"]] then
-	-- 	if LBIS:FindInPhase(phaseText, "4") then
-	--		return true;
-	-- 	end
-	-- end
+	if LBISSettings.PhaseTooltip[LBIS.L["Phase 4"]] then
+		if LBIS:FindInPhase(phaseText, "4") then
+			return true;
+		end
+	end
 	-- if LBISSettings.PhaseTooltip[LBIS.L["Phase 5"]] then
 	-- 	if LBIS:FindInPhase(phaseText, "5") then
 	--		return true;
